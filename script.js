6596491f6348b4e182d3fa2d5c06c7ccdb89f45c
Kaila Santiago <ksantiago.student@hsfi.us>
2016-12-19 22:08:08 +0000

    made a rainbow

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 //Write your function calls below this line
+$(document).ready(function(){
+//red
+    setColor(2, 6, "red");
+    setColor(2, 5, "red");
+    setColor(2, 4, "red");
+    setColor(2, 3, "red");
+    setColor(3, 2, "red");
+    setColor(3, 7, "red");
+    setColor(4, 1, "red");
+    setColor(5, 0, "red");
+    setColor(6, 0, "red");
+    setColor(7, 0, "red");
+    setColor(4, 8, "red");
+    setColor(5, 9, "red");
+    setColor(6, 9, "red");
+    setColor(7, 9, "red");
+//yellow
+    setColor(7, 8, "yellow");
+    setColor(6, 8, "yellow");
+    setColor(5, 8, "yellow");
+    setColor(4, 7, "yellow");
+    setColor(3, 6, "yellow");
+    setColor(3, 5, "yellow");
+    setColor(3, 4, "yellow");
+    setColor(3, 3, "yellow");
+    setColor(4, 2, "yellow");
+    setColor(5, 1, "yellow");
+    setColor(6, 1, "yellow");
+    setColor(7, 1, "yellow");
+//green
+    setColor(7, 7, "green");
+    setColor(6, 7, "green");
+    setColor(5, 7, "green");
+    setColor(4, 6, "green");
+    setColor(4, 5, "green");
+    setColor(4, 4, "green");
+    setColor(4, 3, "green");
+    setColor(5, 2, "green");
+    setColor(6, 2, "green");
+    setColor(7, 2, "green");
+//blue
+    setColor(7, 6, "blue");
+    setColor(6, 6, "blue");
+    setColor(5, 6, "blue");
+    setColor(5, 5, "blue");
+    setColor(5, 4, "blue");
+    setColor(5, 3, "blue");
+    setColor(6, 3, "blue");
+    setColor(7, 3, "blue");
+});
